e9f68eab976baa90ebae434da2b183234dca3362
Jeremiah Chienda <jeremiahchienda@gmail.com>
2023-07-02 07:10:15 +0200

    added core project and solution files

---
+++ MalawiMeta.Api/Program.cs
@@ -0,0 +1,43 @@
+﻿using MalawiMeta.Api.Endpoints.Districts;
+using MalawiMeta.Api.Repositories;
+using MalawiMeta.Api.Services;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+
+// app registrations
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var openApiSection = builder.Configuration.GetSection("OpenApi");
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = openApiSection["Version"] ?? "v1",
+        Title = openApiSection["Name"] ?? "Malawi Meta API",
+        Description = openApiSection["Description"] ?? "A simple API to retrieve Malawi's districts and regions.",
+    });
+});
+
+builder.Services.AddScoped<IDistrictService, DistrictService>();
+builder.Services.AddSingleton<IDistrictRepository, InMemoryDistrictRepository>();
+
+var app = builder.Build();
+/*---------------------------------*/
+// middleware registrations
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    //change swagger endpoint to "/" instead of "/swagger"
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Malawi Meta API");
+    c.RoutePrefix = string.Empty;
+});
+
+/*---------------------------------*/
+// endpoint registrations
+app.MapAllDistricts();
+app.MapDistrictById();
+
+
+app.Run();
