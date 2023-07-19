fe14735954fb716ecf5ef3cd9c2d6e610d1f6e75
TauG <tauraigombera@gmail.com>
2023-07-19 15:05:58 +0200

    added an city entity as an aggregate

---
+++ MalawiMeta.Api/Domain/Aggregates/City.cs
@@ -0,0 +1,29 @@
+﻿using MalawiMeta.Api.Domain.ValueObjects;
+
+namespace MalawiMeta.Api.Domain.Aggregates;
+
+public sealed class City : AuditedAggregateRoot<Guid>
+{
+    public string Name { get; private set; }
+    public Guid DistrictId { get; private set; }
+    public List<Guid> LocationIds { get; private set; }
+    public Geolocation Geolocation { get; private set; }
+    public Population Population { get; private set; }
+
+    private City(Guid id, string name, Guid districtId) : base(id)
+    {
+        Name = name;
+        DistrictId = districtId;
+        Population = new Population(0, 0, 0);
+        Geolocation = new Geolocation(0.0, 0.0);
+        LocationIds = new List<Guid>();
+    }
+    
+    public static City Create(string name, Guid districtId)
+    {
+        City city = new City(Guid.NewGuid(), name, districtId);
+        return city;
+    }
+}
+
+
