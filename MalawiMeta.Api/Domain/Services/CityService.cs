a9b323a842c446986578ca39d4f21629ede43dd5
TauG <tauraigombera@gmail.com>
2023-07-19 18:52:14 +0200

    added a CityService

---
+++ MalawiMeta.Api/Domain/Services/CityService.cs
@@ -0,0 +1,51 @@
+﻿using ErrorOr;
+using MalawiMeta.Api.Domain.Aggregates;
+
+namespace MalawiMeta.Api.Domain.Services;
+
+public class InMemoryCityService : ICityService
+{
+    private readonly IEnumerable<City> _cities;
+
+    public InMemoryCityService()
+    {
+        IEnumerable<City> initialCity = new[]
+        {
+            City.Create("Blantyre", Guid.NewGuid()),
+            City.Create("Zomba", Guid.NewGuid()),
+            City.Create("Mangochi", Guid.NewGuid()),
+            City.Create("Lilongwe", Guid.NewGuid()),
+            City.Create("Mzuzu", Guid.NewGuid()),
+        };
+        _cities = initialCity;
+    }
+
+    public Task<ErrorOr<IEnumerable<City>>> GetCitiesAsync()
+    {
+        return Task.FromResult(ErrorOrFactory.From(_cities));
+    }
+    public Task<ErrorOr<City>> GetCityByIdAsync(Guid id)
+    {
+        var city = _cities.FirstOrDefault(d => d.Id == id);
+
+        if (city == null)
+        {
+            return Task.FromResult<ErrorOr<City>>(Error.NotFound(StatusCodes.Status404NotFound.ToString(), "City not found"));
+        }
+        return Task.FromResult<ErrorOr<City>>(city);
+    }
+    public Task<ErrorOr<City>> AddCityAsync(City city)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<ErrorOr<City>> UpdateCityAsync(City city)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ErrorOr<bool>> DeleteCityAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
+
