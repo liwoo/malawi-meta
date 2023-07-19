3a0bf8f73fe34bc9f6413816d907b82b674f2fbe
TauG <tauraigombera@gmail.com>
2023-07-19 18:57:36 +0200

    implemented City MapAll and MapById Endpoints

---
+++ MalawiMeta.Api/Endpoints/Cities/MapCityApi.cs
@@ -0,0 +1,17 @@
+﻿namespace MalawiMeta.Api.Endpoints.Cities;
+public static partial class CityEndpoints
+{
+    public static void MapCities(this WebApplication app)
+    {
+        app.MapGroup("/api/cities")
+            .MapCitiesApi()
+            .WithTags("Cities");
+    }
+
+    private static RouteGroupBuilder MapCitiesApi(this RouteGroupBuilder group)
+    {
+        group.MapAllCities();
+        group.MapCityById();
+        return group;
+    }
+}
---
+++ MalawiMeta.Api/Endpoints/Cities/MapCityByIdEndpoint.cs
@@ -0,0 +1,58 @@
+﻿using MalawiMeta.Api.Extensions;
+using MalawiMeta.Api.TransferObjects;
+using MalawiMeta.Api.UseCases.Cities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace MalawiMeta.Api.Endpoints.Cities;
+
+public static partial class CityEndpoints
+{
+    private static void MapCityById(this IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+            "/{id}",
+            [ProducesResponseType(typeof(CityResponseDto), StatusCodes.Status200OK)]
+            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+            async (HttpRequest request, HttpResponse response) =>
+                {
+
+                    var context = request.HttpContext;
+                    var id = request.RouteValues["id"];
+                    var path = context.Request.Path;
+
+                    var fetchCityById = await context.GetServiceOrThrowAsync<IFetchCityByIdUseCase>();
+
+                    var result = await fetchCityById.ExecuteAsync(new FetchCityByIdCaseArgs(id?.ToString() ?? string.Empty));
+
+                    result.SwitchFirst(
+                        city => response.WriteAsJsonAsync(city),
+                        error =>
+                        {
+                            var code = int.TryParse(error.Code, out var parsedCode)
+                                ? parsedCode
+                                : StatusCodes.Status500InternalServerError;
+                            response.StatusCode = code;
+                            response.WriteAsJsonAsync(Responses.DefaultErrorResponse(path, error.Type.ToString(),
+                                error.Description, code));
+                        }
+                    );
+
+                })
+            .WithName("GetCityById")
+            .WithDescription("Get a city by its id")
+            .WithOpenApi(generatedOperation =>
+            {
+                generatedOperation.Parameters.Add(new OpenApiParameter
+                {
+                    Name = "id",
+                    In = ParameterLocation.Path,
+                    Required = true,
+                    Description = "The id of the city to be retrieved"
+                });
+
+                return generatedOperation;
+            });
+    }
+}
