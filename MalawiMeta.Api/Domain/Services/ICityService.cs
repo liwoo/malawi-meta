6b52ee2923ccc70b8ddc245bcbfe8680c6406255
TauG <tauraigombera@gmail.com>
2023-07-19 16:10:43 +0200

    added ICityService Interface

---
+++ MalawiMeta.Api/Domain/Services/ICityService.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using MalawiMeta.Api.Domain.Aggregates;
+
+namespace MalawiMeta.Api.Domain.Services;
+
+public interface ICityService
+{
+    public Task<ErrorOr<IEnumerable<City>>> GetCitiesAsync();
+    public Task<ErrorOr<City>> GetCityByIdAsync(Guid id);
+    Task<ErrorOr<City>> AddCityAsync(City city);
+    Task<ErrorOr<City>> UpdateCityAsync(City city);
+    Task<ErrorOr<bool>> DeleteCityAsync(Guid id);
+}
