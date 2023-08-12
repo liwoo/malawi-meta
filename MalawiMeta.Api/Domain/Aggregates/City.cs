b451383e0ee10d7336f75354fcb2cd826413b287
TauG <tauraigombera@gmail.com>
2023-08-12 13:16:46 +0200

    Initialized Geolocation as part of the City

--- MalawiMeta.Api/Domain/Aggregates/City.cs
+++ MalawiMeta.Api/Domain/Aggregates/City.cs
@@ -1,29 +1,37 @@
 ﻿using MalawiMeta.Api.Domain.ValueObjects;
 
-namespace MalawiMeta.Api.Domain.Aggregates;
-
-public sealed class City : AuditedAggregateRoot<Guid>
+namespace MalawiMeta.Api.Domain.Aggregates
 {
-    public string Name { get; private set; }
-    public Guid DistrictId { get; private set; }
-    public List<Guid> LocationIds { get; private set; }
-    public Geolocation Geolocation { get; private set; }
-    public Population Population { get; private set; }
-
-    private City(Guid id, string name, Guid districtId) : base(id)
+    public sealed class City : AuditedAggregateRoot<Guid>
     {
-        Name = name;
-        DistrictId = districtId;
-        Population = new Population(0, 0, 0);
-        Geolocation = new Geolocation(0.0, 0.0);
-        LocationIds = new List<Guid>();
-    }
-    
-    public static City Create(string name, Guid districtId)
-    {
-        City city = new City(Guid.NewGuid(), name, districtId);
-        return city;
+        public string Name { get; private set; }
+        public Guid DistrictId { get; private set; }
+        public List<Guid> LocationIds { get; private set; }
+        public Geolocation Geolocation { get; private set; }
+        public Population? Population { get; private set; }
+        private City(Guid id, string name, Guid districtId, double latitude, double longitude) : base(id)
+        {
+            Name = name;
+            DistrictId = districtId;
+            LocationIds = new List<Guid>();
+            Geolocation = new Geolocation(latitude, longitude);
+        }
+        public static City Create(string name, Guid districtId, double latitude, double longitude)
+        {
+            City city = new City(Guid.NewGuid(), name, districtId, latitude, longitude);
+            return city;
+        }
+        public void SetPopulation(int totalPopulation, int malePopulation, int femalePopulation)
+        {
+            Population = new Population(totalPopulation, malePopulation, femalePopulation);
+        }
+        public void AddLocation(Guid locationId)
+        {
+            LocationIds.Add(locationId);
+        }
+        public void RemoveLocation(Guid locationId)
+        {
+            LocationIds.Remove(locationId);
+        }
     }
 }
-
-
