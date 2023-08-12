43ab7f3fdb7bc9d25aed72dd720df7730a12c230
TauG <tauraigombera@gmail.com>
2023-08-12 11:55:52 +0200

    moved attributes to the end of the route definition

--- MalawiMeta.Api/Endpoints/Cities/MapCityByIdEndpoint.cs
+++ MalawiMeta.Api/Endpoints/Cities/MapCityByIdEndpoint.cs
@@ -10,49 +10,44 @@ public static partial class CityEndpoints
 {
     private static void MapCityById(this IEndpointRouteBuilder app)
     {
-        app.MapGet(
-            "/{id}",
-            [ProducesResponseType(typeof(CityResponseDto), StatusCodes.Status200OK)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-            async (HttpRequest request, HttpResponse response) =>
+        app.MapGet("/{id}", async (HttpRequest request, HttpResponse response) =>
+        {
+            var context = request.HttpContext;
+            var id = request.RouteValues["id"];
+            var path = context.Request.Path;
+
+            var fetchCityById = await context.GetServiceOrThrowAsync<IFetchCityByIdUseCase>();
+
+            var result = await fetchCityById.ExecuteAsync(new FetchCityByIdCaseArgs(id?.ToString() ?? string.Empty));
+
+            result.SwitchFirst(
+                city => response.WriteAsJsonAsync(city),
+                error =>
                 {
+                    var code = int.TryParse(error.Code, out var parsedCode)
+                        ? parsedCode
+                        : StatusCodes.Status500InternalServerError;
+                    response.StatusCode = code;
+                    response.WriteAsJsonAsync(Responses.DefaultErrorResponse(path, error.Type.ToString(),
+                        error.Description, code));
+                }
+            );
 
-                    var context = request.HttpContext;
-                    var id = request.RouteValues["id"];
-                    var path = context.Request.Path;
-
-                    var fetchCityById = await context.GetServiceOrThrowAsync<IFetchCityByIdUseCase>();
-
-                    var result = await fetchCityById.ExecuteAsync(new FetchCityByIdCaseArgs(id?.ToString() ?? string.Empty));
-
-                    result.SwitchFirst(
-                        city => response.WriteAsJsonAsync(city),
-                        error =>
-                        {
-                            var code = int.TryParse(error.Code, out var parsedCode)
-                                ? parsedCode
-                                : StatusCodes.Status500InternalServerError;
-                            response.StatusCode = code;
-                            response.WriteAsJsonAsync(Responses.DefaultErrorResponse(path, error.Type.ToString(),
-                                error.Description, code));
-                        }
-                    );
-
-                })
-            .WithName("GetCityById")
-            .WithDescription("Get a city by its id")
-            .WithOpenApi(generatedOperation =>
+        }).WithName("GetCityById")
+          .WithDescription("Get a city by its id")
+          .WithOpenApi(generatedOperation =>
+        {
+            generatedOperation.Parameters.Add(new OpenApiParameter
             {
-                generatedOperation.Parameters.Add(new OpenApiParameter
-                {
-                    Name = "id",
-                    In = ParameterLocation.Path,
-                    Required = true,
-                    Description = "The id of the city to be retrieved"
-                });
-
-                return generatedOperation;
+                Name = "id",
+                In = ParameterLocation.Path,
+                Required = true,
+                Description = "The id of the city to be retrieved"
             });
+
+            return generatedOperation;
+        }).Produces<CityResponseDto> (StatusCodes.Status200OK)
+          .Produces<ProblemDetails> (StatusCodes.Status500InternalServerError)
+          .Produces<ProblemDetails> (StatusCodes.Status404NotFound);
     }
 }
