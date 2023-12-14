78118793a7a00bc2fe97b9ef3248f008b0c783e5
Jeremiah Chienda <jeremiahchienda@gmail.com>
2023-12-14 23:02:47 +0200

    added `DistrictId` to Domain and DTO

--- MalawiMeta.Api/Repositories/DistrictRepository.cs
+++ MalawiMeta.Api/Repositories/DistrictRepository.cs
@@ -18,19 +18,19 @@ public class InMemoryDistrictRepository : IDistrictRepository
     {
         IEnumerable<District> initialDistrict = new[]
         {
-            District.Create("Blantyre", "BT", new RegionId(Guid.NewGuid())),
-            District.Create("Chikwawa", "CK", new RegionId(Guid.NewGuid())),
-            District.Create("Chiradzulu", "CZ", new RegionId(Guid.NewGuid())),
-            District.Create("Machinga", "MCH", new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("6bac69be-b292-415c-af81-4549fa99cfcb"),  "Blantyre", "BT", new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("c3fdfa83-8c18-413e-bc4e-9fc4d0b1f58e"),"Chikwawa", "CK", new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("39677429-fef1-49e3-bf6c-a642071ed6da"), "Chiradzulu", "CZ", new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("5dc1fc15-014c-4be9-ba04-4a5f83a9039c"), "Machinga", "MCH", new RegionId(Guid.NewGuid())),
             //few from the central region
-            District.Create("Dedza", "DZ",new RegionId(Guid.NewGuid())),
-            District.Create("Dowa", "DA", new RegionId(Guid.NewGuid())),
-            District.Create("Kasungu", "KU",new RegionId(Guid.NewGuid())),
-            District.Create("Lilongwe", "LL",new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("7a103a06-74c1-4c1a-b50a-5cf71dd3d510"), "Dedza", "DZ",new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("ca3bb6db-8f31-439a-a9fd-380a9f5c3287"),"Dowa", "DA", new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("2a50c886-cea2-4c73-8ea6-264b5ee157a7"),"Kasungu", "KU",new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("603f16c3-0eed-4de1-bf3f-074e04e05033"), "Lilongwe", "LL",new RegionId(Guid.NewGuid())),
             //few from the northern region
-            District.Create("Chitipa", "CP", new RegionId(Guid.NewGuid())),
-            District.Create("Karonga", "KA",new RegionId(Guid.NewGuid())),
-            District.Create("Likoma", "LK",new RegionId(Guid.NewGuid()))
+            District.Create(Guid.Parse("2b0f1e85-abba-4715-a935-88c3beac32f2"), "Chitipa", "CP", new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("ca35c476-7c88-4c7f-a397-1e2ede30c1a4"), "Karonga", "KA",new RegionId(Guid.NewGuid())),
+            District.Create(Guid.Parse("0a8d99ac-e161-459e-a719-76992b72e8c2"),"Likoma", "LK",new RegionId(Guid.NewGuid()))
         };
         _districts = initialDistrict;
     }
@@ -46,8 +46,7 @@ public class InMemoryDistrictRepository : IDistrictRepository
 
         return district switch
         {
-            null => Task.FromResult<ErrorOr<District>>(Error.NotFound(StatusCodes.Status404NotFound.ToString(), 
-                "District not found")),
+            null => Task.FromResult<ErrorOr<District>>(Error.NotFound()),
             _ => Task.FromResult<ErrorOr<District>>(district)
         };
     }
