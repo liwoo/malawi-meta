304b778d577dac08e5de9d9103e705fa89a77860
Jeremiah Chienda <jeremiahchienda@gmail.com>
2023-12-14 23:01:37 +0200

    refactored `AllDistrictsEndpoint` and `DistrictByIdEndpoint` to .NET MinimalAPI default syntax

---
+++ MalawiMeta.Api/Endpoints/Districts/AllDistrictsEndpoint.cs
@@ -0,0 +1,29 @@
+using ErrorOr;
+using MalawiMeta.Api.Extensions;
+using MalawiMeta.Api.TransferObjects;
+using MalawiMeta.Api.UseCases.Districts;
+
+namespace MalawiMeta.Api.Endpoints.Districts;
+
+public static partial class DistrictEndpoints
+{
+    private static void MapAllDistrictsEndpoint(this IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+                "/",
+                async (IFetchAllDistrictsUseCase fetchAllDistricts) =>
+                {
+                    var result = await fetchAllDistricts.ExecuteAsync(null);
+
+                    return result switch {
+                        {IsError: true} => Results.Problem(statusCode: StatusCodes.Status500InternalServerError,
+                            detail: result.Errors.First().Description),
+                        {Value: not null} => Results.Json(result.Value),
+                        _ => Results.NotFound()
+                    };
+                })
+            .Produces(StatusCodes.Status200OK, responseType: typeof(IEnumerable<DistrictResponseDto>))
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithName("GetDistricts");
+    }
+}
---
+++ MalawiMeta.Api/Endpoints/Districts/DistrictByIdEndpoint.cs
@@ -0,0 +1,41 @@
+using ErrorOr;
+using MalawiMeta.Api.Extensions;
+using MalawiMeta.Api.TransferObjects;
+using MalawiMeta.Api.UseCases.Districts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace MalawiMeta.Api.Endpoints.Districts;
+
+public static partial class DistrictEndpoints
+{
+    private static void MapDistrictByIdEndpoint(this IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+                "/{id:guid}",
+                async (Guid id, IFetchDistrictByIdUseCase fetchDistrictById) =>
+                {
+                    var useCaseResult = await fetchDistrictById.ExecuteAsync(new FetchDistrictByIdCaseArgs(id));
+
+                    return useCaseResult switch
+                    {
+                        {IsError: true} => useCaseResult.Errors.First() switch
+                        {
+                            {Type: ErrorType.Validation} => Results.Problem(statusCode: StatusCodes.Status400BadRequest,
+                                detail: useCaseResult.Errors.First().Description),
+                            {Type: ErrorType.NotFound} => Results.Problem(statusCode: StatusCodes.Status404NotFound,
+                                detail: useCaseResult.Errors.First().Description),
+                            _ => Results.Problem(statusCode: StatusCodes.Status500InternalServerError,
+                                detail: useCaseResult.Errors.First().Description)
+                        },
+                        {Value: not null} => Results.Json(useCaseResult.Value),
+                        _ => Results.NotFound()
+                    };
+                })
+            .WithName("GetDistrictById")
+            .WithDescription("Get a district by its id")
+            .Produces(StatusCodes.Status200OK, responseType: typeof(DistrictResponseDto))
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status500InternalServerError);
+    }
+}
