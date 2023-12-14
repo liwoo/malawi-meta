3b5f95eb800c2b39d0df04716a2a7bcf5869f22d
Jeremiah Chienda <jeremiahchienda@gmail.com>
2023-12-14 23:05:10 +0200

    refactored to C#12 `Primary Constructors`

--- MalawiMeta.Api/UseCases/Districts/FetchAllDistrictsUseCase.cs
+++ MalawiMeta.Api/UseCases/Districts/FetchAllDistrictsUseCase.cs
@@ -4,29 +4,20 @@ using MalawiMeta.Api.TransferObjects;
 
 namespace MalawiMeta.Api.UseCases.Districts;
 
+public interface IFetchAllDistrictsUseCase : IUseCase<object, ErrorOr<IEnumerable<DistrictResponseDto>>>;
 
-public interface IFetchAllDistrictsUseCase : IUseCase<object, ErrorOr<IEnumerable<DistrictResponseDto>>>
+public class FetchAllDistrictsUseCase(IDistrictRepository districtRepository) : IFetchAllDistrictsUseCase
 {
-}
-
-public class FetchAllDistrictsUseCase : IFetchAllDistrictsUseCase
-{
-    private readonly IDistrictRepository _districtRepository;
-
-    public FetchAllDistrictsUseCase(IDistrictRepository districtRepository)
-    {
-        _districtRepository = districtRepository;
-    }
-
     public async Task<ErrorOr<IEnumerable<DistrictResponseDto>>> ExecuteAsync(object? args)
     {
-        var districtResult = await _districtRepository.GetDistrictsAsync();
+        var districtResult = await districtRepository.GetDistrictsAsync();
 
         return districtResult.IsError switch
         {
             true => districtResult.FirstError,
-            _ => districtResult.Value.Select(d => new DistrictResponseDto(d.Name, d.Code, d.RegionId.ToString())).ToList()
+            _ => districtResult.Value
+                .Select(d => new DistrictResponseDto(d.Id, d.Name, d.Code, d.RegionId.Value.ToString()))
+                .ToList()
         };
-        
     }
 }
--- MalawiMeta.Api/UseCases/Districts/FetchDistrictByIdUseCase.cs
+++ MalawiMeta.Api/UseCases/Districts/FetchDistrictByIdUseCase.cs
@@ -4,36 +4,27 @@ using MalawiMeta.Api.TransferObjects;
 
 namespace MalawiMeta.Api.UseCases.Districts;
 
-public record FetchDistrictByIdCaseArgs(string Id);
+public record FetchDistrictByIdCaseArgs(Guid Id);
 
-public interface IFetchDistrictByIdUseCase : IUseCase<FetchDistrictByIdCaseArgs, ErrorOr<DistrictResponseDto>>
+public interface IFetchDistrictByIdUseCase : IUseCase<FetchDistrictByIdCaseArgs, ErrorOr<DistrictResponseDto>>;
+
+public class FetchDistrictByIdUseCase(IDistrictRepository districtRepository) : IFetchDistrictByIdUseCase
 {
-}
-
-
-public class FetchDistrictByIdUseCase : IFetchDistrictByIdUseCase
-{
-    private readonly IDistrictRepository _districtRepository;
-
-    public FetchDistrictByIdUseCase(IDistrictRepository districtRepository)
-    {
-        _districtRepository = districtRepository;
-    }
-
     public async Task<ErrorOr<DistrictResponseDto>> ExecuteAsync(FetchDistrictByIdCaseArgs? args)
     {
-        var guuidId = Guid.TryParse(args?.Id, out var guuid) ? guuid : Guid.Empty;
-        if (guuidId == Guid.Empty)
+        var id = args?.Id;
+        if (id == null)
         {
-            return Error.Validation(description: "ID is not a valid GUID", code: StatusCodes.Status400BadRequest.ToString());
+            return Error.Validation();
         }
-        
-        var districtResult = await _districtRepository.GetDistrictByIdAsync(guuidId);
-        
+
+        var districtResult = await districtRepository.GetDistrictByIdAsync(id.Value);
+
         return districtResult.IsError switch
         {
             true => districtResult.FirstError,
-            _ => new DistrictResponseDto(districtResult.Value.Name, districtResult.Value.Code, districtResult.Value.RegionId.ToString())
+            _ => new DistrictResponseDto(districtResult.Value.Id, districtResult.Value.Name, districtResult.Value.Code,
+                districtResult.Value.RegionId.Value.ToString())
         };
     }
 }
